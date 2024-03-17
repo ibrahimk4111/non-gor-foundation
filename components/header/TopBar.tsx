a663da83003824503322e3f44d3d6760fc84dd1b
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-17 16:21:45 +0600

    react-icons added

---
+++ components/header/TopBar.tsx
@@ -0,0 +1,16 @@
+const TopBar:React.FC = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+        facebook 
+        youtube
+        mail
+        call 
+        eng | bn
+    </div>
+  )
+}
+
+export default TopBar
