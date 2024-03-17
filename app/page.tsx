18dc601d3c64593e172f4f758ce4fdeda5688b14
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-18 02:59:58 +0600

    full page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Gallery from "@/components/Gallery/Gallery";
 import About from "@/components/about/About";
 import Donate from "@/components/donate/Donate";
 import News from "@/components/news/News";
 import OnGoinProjects from "@/components/onGoinProjects/OnGoinProjects";
+import Projects from "@/components/projects/Projects";
+import Videos from "@/components/videos/Videos";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
         <Donate />
         <OnGoinProjects />
         <News />
+        <Projects />
+        <Videos />
+        <Gallery />
     </main>
   );
 }
--- components/videos/Videos.tsx
+++ components/videos/Videos.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+// import VideoCards from './videoCards/VideoCards'
 
-const Videos = () => {
+const Videos: React.FC = () => {
+  const videoId = "7R0geoRoIn55f662";
   return (
-    <div>Videos</div>
-  )
-}
+    <div className=" py-10 bg-white ">
+      <div className=" max-w-[1280px] mx-auto">
+        <h1 className=" text-2xl text-center mb-10">Videos</h1>
+        <div className=" grid lg:grid-cols-3 gap-5 ">
+          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/-AtfFUeeuIs?si=7R0geoRoIn55f662" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+          {/* <ReactPlayer url='https://www.youtube.com/watch?v=wWgIAphfn2U' />  */}
+          {/* <VideoCards videoId={videoId} /> */}
+          {/* <video src="https://www.youtube.com/embed/-AtfFUeeuIs?si=7R0geoRoIn55f662"></video> */}
+          {/* <video controls={true}>
+                <source src="www.youtube.com/watch?v=IEDEtZ4UVtI" type="video/mp4">
+              </video> */}
+          videos will come here
+        </div>
+        <div className=" flex justify-center items-center py-5 mt-5">
+          <button className="py-3 px-32 text-white bg-green-700 rounded-lg ">
+            More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Videos
+export default Videos;
