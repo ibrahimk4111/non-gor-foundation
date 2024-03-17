9aa08a03ef09d33ef503a1e78b9572d64a82b125
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-18 02:20:29 +0600

    all cards added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import About from "@/components/about/About";
+import Donate from "@/components/donate/Donate";
+import News from "@/components/news/News";
+import OnGoinProjects from "@/components/onGoinProjects/OnGoinProjects";
 
 export default function Home() {
   return (
-    <main className=" max-w-[1280px] mx-auto">
+    <main>
         <About />
+        <Donate />
+        <OnGoinProjects />
+        <News />
     </main>
   );
 }
---
+++ components/news/News.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NewsCardsCom from "./newsCards/NewsCardsCom";
+
+const News = () => {
+  return (
+    <div className=" py-10 bg-slate-100 ">
+      <div className=" max-w-[1280px] mx-auto">
+        <h1 className=" text-2xl text-center mb-10">On Going Projects</h1>
+        <div className=" grid lg:grid-cols-3 gap-5 ">
+          <NewsCardsCom />
+          <NewsCardsCom />
+          <NewsCardsCom />
+        </div>
+        <div className=" flex justify-center items-center py-5 mt-5">
+          <button className="py-3 px-32 text-white bg-green-700 rounded-lg ">
+            More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;
