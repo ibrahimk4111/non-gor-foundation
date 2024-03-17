1361f5b0c93fe07962f515a400ae7548dd6270ce
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-18 03:22:28 +0600

    video component added

--- components/videos/Videos.tsx
+++ components/videos/Videos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import VideoCards from './videoCards/VideoCards'
+import ImageComp from "../Gallery/imageComp/ImageComp";
 
 const Videos: React.FC = () => {
   const videoId = "7R0geoRoIn55f662";
@@ -8,14 +8,9 @@ const Videos: React.FC = () => {
       <div className=" max-w-[1280px] mx-auto">
         <h1 className=" text-2xl text-center mb-10">Videos</h1>
         <div className=" grid lg:grid-cols-3 gap-5 ">
-          {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/-AtfFUeeuIs?si=7R0geoRoIn55f662" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-          {/* <ReactPlayer url='https://www.youtube.com/watch?v=wWgIAphfn2U' />  */}
-          {/* <VideoCards videoId={videoId} /> */}
-          {/* <video src="https://www.youtube.com/embed/-AtfFUeeuIs?si=7R0geoRoIn55f662"></video> */}
-          {/* <video controls={true}>
-                <source src="www.youtube.com/watch?v=IEDEtZ4UVtI" type="video/mp4">
-              </video> */}
-          videos will come here
+          <ImageComp />
+          <ImageComp />
+          <ImageComp />
         </div>
         <div className=" flex justify-center items-center py-5 mt-5">
           <button className="py-3 px-32 text-white bg-green-700 rounded-lg ">
