d5a47342f235ce44e24c8500fc2d87998bba34fa
Sabithasan2098 <sabithasan2098@gmail.com>
2024-03-17 15:56:00 +0600

    only change text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-        <h2 className=" text-center mt-10 text-2xl">Clone As-sunnah Foundation</h2>
+      <h2 className=" text-center mt-10 text-2xl">Ready to complete this project</h2>
     </main>
   );
 }
