34ce85910e46f9c8e9086c56450b72dd10ef5235
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-03-21 23:11:37 +0600

    contact page completed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,126 +1,30 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  email: z.string().min(2, {
-    message: "example@gmail.com",
-  }),
-  number: z.number().min(2, {
-    message: "Minimum Donation 10 taka.",
-  }),
-});
+import ContactForm from "@/components/contactForm/ContactForm";
 
 const page: React.FC = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-      email: "",
-      number: 10,
-    },
-  });
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log("form values");
-  }
-
   return (
     <section className="my-8 lg:px-5">
       <div className=" container mx-auto ">
         <div className=" grid lg:grid-cols-2 grid-cols-1 gap-5 ">
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className=" flex flex-col justify-start items-start md:gap-5 gap-2 border-2 rounded-md bg-white p-5"
-            >
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        placeholder="Your Name"
-                        {...field}
-                        className=" text-xl "
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        placeholder="Email / Phone"
-                        {...field}
-                        className=" text-xl "
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="number"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        placeholder="Donation Amount"
-                        {...field}
-                        className=" text-xl "
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <div>
-                <button
-                  type="submit"
-                  className="py-2 px-5 text-white bg-green-700 rounded-md "
-                >
-                  Donate
-                </button>
-              </div>
-            </form>
-          </Form>
+          <ContactForm />
           <section>
-            <div className=" border-2 rounded-lg mb-5 p-5 "> 
-                <h1 className=" text-xl font-semibold py-2" >Where we are ?</h1>
-                <div className=" h-60 w-full ">
-                    here
-                </div>
+            {/* map section */}
+            <div className=" border-2 rounded-lg mb-5 p-5 ">
+              <h1 className=" text-xl font-semibold mb-5">Where we are ?</h1>
+              <div className=" h-72">
+                <iframe
+                  className=" rounded-lg w-full min-h-full h-52 "
+                  title="Arifs (Bangladesh) Ltd"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.5551060372236!2d90.41215227425799!3d23.72757648965419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8582cff629d%3A0xd646e704f2db23e3!2sArifs%20(BD)%20Ltd%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1705415346424!5m2!1sen!2sbd"
+                  allowFullScreen={true}
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
             </div>
-            <div  className=" border-2 rounded-lg mb-5 p-5 flex flex-col gap-3 ">
-              <h1 className=" text-xl font-semibold" >Contact Details</h1>
+
+            {/* contact address section */}
+            <div className=" border-2 rounded-lg mb-5 p-5 flex flex-col gap-3 ">
+              <h1 className=" text-xl font-semibold">Contact Details</h1>
               <hr className=" h-1 " />
               <div>
                 <h1 className=" text-xl font-semibold py-2">Address</h1>
---
+++ components/contactForm/ContactForm.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.string().min(2, {
+    message: "Enter a valid email address. ",
+  }),
+  number: z.string().min(11, {
+    message: "Enter 11 digits phone numebr. ",
+  }),
+  message: z.string(),
+});
+
+interface formDataType {
+  name: string;
+  email: string;
+  number: string;
+  message: string;
+}
+
+const formFieldData = [
+  { fieldName: 'name', label: "Your Name", placeholder: "Latent Talent" },
+  { fieldName: 'email', label: "Email", placeholder: "example@gmail.com" },
+  { fieldName: 'number', label: "Phone Number", placeholder: "Phone Number" },
+];
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState<formDataType>({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      number: "",
+      message: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setFormData(values);
+  }
+
+  return (
+    <div>
+      <h1 className=" text-xl font-semibold lg:pt-5">
+        Feel free to contact us
+      </h1>
+      <hr className=" h-1 my-5" />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" flex flex-col md:gap-5 gap-2 w-full"
+        >
+          {formFieldData.map((fieldData, index) => (
+            <FormField
+              key={index}
+              control={form.control}
+              name={fieldData.fieldName}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className=" text-xl text-slate-500">
+                    {fieldData.label} <span className=" text-red-500 ">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder={fieldData.placeholder}
+                      {...field}
+                      className=" text-xl focus-visible:outline-none"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem className=" flex flex-col">
+                <FormLabel className=" text-xl text-slate-500">
+                  Message
+                </FormLabel>
+                <FormControl>
+                  <textarea
+                    placeholder="Type your message here."
+                    rows={7}
+                    {...field}
+                    className=" text-xl border rounded-md p-2 focus-visible:outline-none"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div>
+            <button
+              type="submit"
+              className="py-2 px-5 text-white bg-green-700 rounded-md "
+            >
+              Sent
+            </button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default ContactForm;
