380d0e0d5ff70d11021ad03bbc383917c5721375
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-04-08 17:38:02 +0600

    top bar flexible

--- components/header/headerComponents/TopBar.tsx
+++ components/header/headerComponents/TopBar.tsx
@@ -7,18 +7,18 @@ const TopBar: React.FC = () => {
   let size = 15;
   return (
     <section className=" pt-2 bg-white ">
-      <div className=" flex justify-end items-center gap-x-10 container mx-auto ">
-        <div className=" flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
+      <div className=" flex sm:flex-row flex-col justify-end md:items-center items-end lg:gap-x-10 gap-1 container mx-auto ">
+        <div className=" hidden sm:flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
           <AiFillMail size={size} />
-          <span className=" text-sm ">nongorfoundation@gmail.com</span>
+          <span className=" text-xs ">nongorfoundation@gmail.com</span>
         </div>
         <div className=" flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
-          <IoCall size={size} />
-          <span className=" text-sm ">01700-000 000</span>
+          <IoCall size={size}/>
+          <span className=" text-xs ">01700-000 000</span>
         </div>
-        <div className=" flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
+        <div className=" hidden sm:flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
           <ExternalLink size={size} />
-          <span className=" text-sm ">Share</span>
+          <span className=" text-xs ">Share</span>
         </div>
       </div>
     </section>
