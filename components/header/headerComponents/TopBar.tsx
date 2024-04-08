7503a24734a5a77c36bfb0439ff4e98f817cc9a5
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-04-08 17:25:19 +0600

    topbar fixs

--- components/header/headerComponents/TopBar.tsx
+++ components/header/headerComponents/TopBar.tsx
@@ -1,16 +1,20 @@
+'use client'
+
 import { FaYoutube } from "react-icons/fa6";
 import { AiFillMail } from "react-icons/ai";
 import { IoCall } from "react-icons/io5";
 import { FaFacebookSquare } from "react-icons/fa";
 import EngBnToggle from "./EngBnToggle";
 import Link from "next/link";
+import { ExternalLink, ShareIcon } from "lucide-react";
 
 const TopBar: React.FC = () => {
-  let size = 20;
+
+  let size = 15;
   return (
     <section className=" pt-2 bg-white ">
-      <div className=" flex justify-end items-center gap-2 container mx-auto ">
-        <Link href="https://facebook.com" target="_blank">
+      <div className=" flex justify-end items-center gap-x-10 container mx-auto ">
+        {/* <Link href="https://facebook.com" target="_blank">
           <FaFacebookSquare size={size} fill="blue " />
         </Link>
 
@@ -26,7 +30,19 @@ const TopBar: React.FC = () => {
           <IoCall size={size} fill=" green " />
         </Link>
 
-        <EngBnToggle />
+        <EngBnToggle /> */}
+        <div className=" flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
+          <AiFillMail size={size} />
+          <span className=" text-sm ">nongorfoundation@gmail.com</span>
+        </div>
+        <div className=" flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
+          <IoCall size={size} />
+          <span className=" text-sm ">01700-000 000</span>
+        </div>
+        <div className=" flex items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer">
+          <ExternalLink size={size} />
+          <span className=" text-sm ">Share</span>
+        </div>
       </div>
     </section>
   );
